f7bcd6a1972edff286868882363f72935a62c340
Penny Simitsi <18515231+eimon96@users.noreply.github.com>
2020-09-17 00:38:35 +0300

    Update 3_Largest_prime_factor.py

--- 3_Largest_prime_factor.py
+++ 3_Largest_prime_factor.py
@@ -3,42 +3,29 @@
 #What is the largest prime factor of the number 600851475143 ?
 #Python 2.7.9
 
+from math import sqrt
+
 def prime(x):
     r = True
-    for i in range(2,x-1,1):
+    n = sqrt(x)
+    i = 2
+    while i <= n:
         if x%i==0:
             r = False
+            break
+        i += 1
     return (r)
 
-num=600851475143//2
-maxx=1
-i=2
+
+num = sqrt(600851475143)
+maxx = 0
+i = 1
 while i<num:
-    if 600851475143%i==0 and prime(i)==True:
-        maxx = i
-    i += 1
+    if 600851475143%i==0:
+        if prime(i):
+            maxx = i
+    i += 2
 
 print maxx
     
-
-#def prime(x):
-#    r = True
-#    n = (x-1)//2
-#    for i in range(2,n,1):
-#        if x%i==0:
-#            r = False
-#            break
-#    return (r)
-#
-#num=600851475143//2
-#maxx=1
-#i=2
-#while i<num:
-#    if prime(i)==True:
-#        if 600851475143%i==0:
-#            maxx = i
-#            print maxx
-#    i += 1
-#
-#print maxx
     
